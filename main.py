5145ec4ceb55196a8becfb460b16b9eb9016c939
Sourabh <sourabhsheokand945@gmail.com>
2024-03-09 19:04:39 +0530

    new file:   data2.csv 	modified:   main.py

--- main.py
+++ main.py
@@ -2,40 +2,79 @@ import pandas as pd
 import os
 import numpy as np
 import shutil
-import matplotlib.pyplot as plt
+
+import csv
 
 def open_file(filepath,type="csv",separator=";") -> None:
-    if type == "csv":
-        with open(filepath,'r') as file:
-            data = pd.read_csv(file,sep=separator,header=None)
-            file.close()
-        return data
-    else:
-        base_name , _  = os.path.splitext(filepath)
-        shutil.copy2(filepath,f'{base_name}.csv')
-        with open(filepath,'r') as file:
-            data = pd.read_csv(file,sep=separator,header=None)
-            file.close()
-        return data
+    try:
+        if type == "csv":
+            with open(filepath,'r') as file:
+                data = pd.read_csv(file,sep=separator,header=None)
+                file.close()
+            return data
+        else:
+            base_name , _  = os.path.splitext(filepath)
+            shutil.copy2(filepath,f'{base_name}.csv')
+            with open(filepath,'r') as file:
+                data = pd.read_csv(file,sep=separator,header=None)
+                file.close()
+            return data
+    except Exception as error:
+        print(error)
 
-class plot:    
+class plot:  
     def __init__(self,x,y) -> None:
+        import matplotlib.pyplot as plt  
         self.plot_object = plt
-        self.plot_object.ion()
-        self.plot_object.plot(x,y)
+        self.x = x
+        self.y = y
     def show(self,xlabel="x",ylabel="y"):
         self.plot_object.xlabel(xlabel)
         self.plot_object.ylabel(ylabel)
+        self.plot_object.plot(self.x,self.y)
         self.plot_object.show()
     def save(self,name):
+        self.plot_object.plot(self.x,self.y)
         self.plot_object.savefig(f"/plots/{name}.png")
 
-def 
+def export_csv(filename,*args) -> bool:
+    try:
+        with open(filename,"w") as file:
+            writer = csv.writer(file)
+            data = []
+            for i in args:
+                data.append(list(i))
+            row = data.__len__()
+            col = data[0].__len__()
+            for i in range(0,col):
+                temp = []
+                for j in range(0,row):
+                    temp.append(data[j][i])
+                writer.writerow(temp)
+        return True
+    except Exception as error:
+        print(error)
+        return False
+    
+class xrd_data:
+    def __init__(self,angle,intensity) -> None:
+        self.angle = angle
+        self.intensity = intensity
+    def peak_separator(self,range) -> list:
+        left_index = np.searchsorted(self.x,range[0],side='left')
+        right_index = np.searchsorted(self.x,range[1],side='right')
+        data = []
+        temp 
+        return 
+    
 
 angle = np.array(open_file("data.csv",separator="\t")[0])
 intensity = np.array(open_file("data.csv",separator="\t")[1])
 
 a = plot(angle,intensity)
-a.show()
+# a.show()
+print(np.searchsorted(angle,10))
+b = plot([0,1,2,3],[0,2,4,6])
+# b.show()
 
-print(plt.isinteractive())
+# a.show()
